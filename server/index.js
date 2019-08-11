db09f14d16ee887703678e95de30bc4918209864
teddy <theskyend0@gmail.com>
2019-08-11 09:47:06 -0700

    complete UI

--- server/index.js
+++ server/index.js
@@ -5,8 +5,24 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+
+let db = {};
+
 app.prepare().then(() => {
     const server = express()
+
+
+    server.post('/api/schedule/save', ( req, res, next ) => {
+
+    });
+
+    server.post('/api/schedule/delete', ( req, res, next ) => {
+
+    });
+
+    server.get('/api/schedule/all', ( req, res, next ) => {
+
+    });
 
 
     server.get('*', (req, res) => {
