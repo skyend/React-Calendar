9bdd63fd7f456cb2b6203bbf41ae66b707ab2363
teddy <theskyend0@gmail.com>
2019-08-12 08:11:14 -0700

    finally complete

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,8 @@ module.exports = {
     apps : [{
         name: "calendar",
         script: "./server/index.js",
-        mode:"cluster",
-        instance:2,
+        exec_mode:"cluster",
+        instances:2,
         env: {
             NODE_ENV: "development",
         },
